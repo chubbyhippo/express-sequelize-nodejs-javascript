3baba683f5424d6cb97beb441129a292bc891813
chubbyhippo <mkmsacc@outlook.com>
2024-12-11 21:20:14 +0700

    Refactor app setup and export for modularity
    Replaced inline app initialization with a router for better modularity. Removed the direct server setup to enable flexibility in using the app module. This prepares the codebase for future extension or testing.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-import express from 'express'
-import * as console from 'node:console';
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+export default app;
