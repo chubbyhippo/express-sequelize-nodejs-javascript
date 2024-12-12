dfea4cf38c1f5ed9f20c4ede5dacd01c978ccb22
chubbyhippo <mkmsacc@outlook.com>
2024-12-12 21:06:27 +0700

    Refactor tests to improve structure and assertion clarity
    Replaced inline `expect` chaining with separate `expect` statements for better readability. Added new assertion to check for the presence of the 'message' property in the response body. These changes improve test clarity and maintainability.

--- test/UserRegister.test.js
+++ test/UserRegister.test.js
@@ -5,18 +5,23 @@ import app from '../app';
 
 describe('User registration test', () => {
   it('should return hello world', async () => {
-    const response = await request(app).get('/')
-      .expect(200);
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(200);
     expect(response.text).toBe('Hello World!');
   });
 
   it('should return status 200 when signup request is valid', async () => {
+
     const response = await request(app).post('/app/users')
       .send({
         username: 'test',
         email: 'test@test.com',
         password: 'password',
-      }).expect(200);
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('message');
     expect(response.body.message).toBe('User created');
   });
 });
