376479d0ee33c6330946316b1085aaadaae65f56
chubbyhippo <mkmsacc@outlook.com>
2024-12-12 21:18:24 +0700

    Add database configuration with Sequelize and SQLite
    Introduced a new `config/database.js` file to initialize Sequelize with SQLite as the database. This sets up a foundation for database operations using SQLite stored in `database.sqlite`.

---
+++ config/database.js
@@ -0,0 +1,8 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+});
+
+export default sequelize;
