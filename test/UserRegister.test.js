d6466501908e1784f45e29a80b04112d47372003
chubbyhippo <mkmsacc@outlook.com>
2024-12-12 20:57:18 +0700

    Refactor user registration tests to use describe block
    Wrapped test cases in a `describe` block for better organization and readability. This improves test structure and aligns with common testing practices.

--- test/UserRegister.test.js
+++ test/UserRegister.test.js
@@ -1,20 +1,22 @@
 import request from 'supertest';
-import { it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 import app from '../app';
 
-it('should return hello world', async () => {
-  const response = await request(app).get('/')
-    .expect(200);
-  expect(response.text).toBe('Hello World!');
-});
+describe('User registration test', () => {
+  it('should return hello world', async () => {
+    const response = await request(app).get('/')
+      .expect(200);
+    expect(response.text).toBe('Hello World!');
+  });
 
-it('should return status 200 when signup request is valid', async () => {
-  const response = await request(app).post('/app/users')
-    .send({
-      username: 'test',
-      email: 'test@test.com',
-      password: 'password',
-    }).expect(200);
-  expect(response.body.message).toBe('User created');
+  it('should return status 200 when signup request is valid', async () => {
+    const response = await request(app).post('/app/users')
+      .send({
+        username: 'test',
+        email: 'test@test.com',
+        password: 'password',
+      }).expect(200);
+    expect(response.body.message).toBe('User created');
+  });
 });
