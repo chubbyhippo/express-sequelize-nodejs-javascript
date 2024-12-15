2e2802a3644a0b0bc4c8a74bac5b4510ee9d1309
chubbyhippo <mkmsacc@outlook.com>
2024-12-15 12:06:58 +0700

    Remove force option from User model sync call
    The `force: true` option was removed to prevent dropping and recreating the table on every app restart. This ensures data persistence and avoids unnecessary disruptions in a production environment.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import User from './src/user.js';
 const port = 8080;
 
 app.listen(port, async () => {
-  await User.sync({ force: true });
+  await User.sync();
   console.log(`Example app listening on port ${port}`);
 });
