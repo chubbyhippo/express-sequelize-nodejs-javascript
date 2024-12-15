ebb44c79c152011a4b8092bc9cd2ac35d3ed82e8
chubbyhippo <mkmsacc@outlook.com>
2024-12-15 13:45:37 +0700

    Refactor User model definition to remove Model inheritance
    Replaced `Model` inheritance with `sequelize.define` for defining the User model. This simplifies the code and removes unnecessary usage of the `Model` class.

--- src/user.js
+++ src/user.js
@@ -1,19 +1,10 @@
-import { DataTypes, Model } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import sequelize from './database.js';
 
-
-class User extends Model {
-}
-
-User.init({
+const User = sequelize.define('user', {
   username: DataTypes.STRING,
   password: DataTypes.STRING,
   email: DataTypes.STRING,
-}, {
-  sequelize,
-  modelName: 'user',
 });
-
-console.log(User === sequelize.models.User);
 
 export default User;
