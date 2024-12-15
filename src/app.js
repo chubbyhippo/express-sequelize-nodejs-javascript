e79ec4c21e1b90c5c7a578dfc9ca63e2338abbe9
chubbyhippo <mkmsacc@outlook.com>
2024-12-15 13:24:31 +0700

    Add JSON middleware and log created user in POST route
    Ensure request bodies are parsed as JSON by adding `express.json()` middleware. Log the created user object to the console in the user creation endpoint for debugging purposes.

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import User from './user.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.post('/app/users', async (req, res) => {
-  await User.create(req.body);
+  const user = await User.create(req.body);
+  console.log(user);
   res.send({ message: 'User created' });
 });
 
