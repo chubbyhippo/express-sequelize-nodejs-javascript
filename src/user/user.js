5a97315987ae70717d5b6bd4264b81d482b3f4a5
ch <mkmsacc@outlook.com>
2024-12-16 10:52:35 +0700

    Add password hashing to User model before creation
    Integrated bcrypt to hash user passwords before saving them to the database. This ensures enhanced security by safeguarding sensitive user data during account creation.

--- src/user/user.js
+++ src/user/user.js
@@ -1,10 +1,16 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../shared/database.js';
+import bcrypt from 'bcrypt';
 
 const User = sequelize.define('user', {
   username: DataTypes.STRING,
   password: DataTypes.STRING,
   email: DataTypes.STRING,
+});
+
+User.beforeCreate((user) => {
+  const saltRounds = 10;
+  user.password = bcrypt.hashSync(user.password, saltRounds);
 });
 
 export default User;
