a1c46c7c77c8b4e2aedd4686d5da0d91f05f6d2d
ch <mkmsacc@outlook.com>
2024-12-16 12:54:14 +0700

    Refactor user routes to use dedicated router module
    Replaced the direct user route handling in `app.js` with a dedicated `userRouter` module to improve code organization and modularity. Removed redundant imports and streamlined the application's routing logic.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 import express from 'express';
-import User from './user/user.js';
-import console from 'node:console';
+import userRouter from './user/user.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(userRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.post('/api/users', async (req, res) => {
-  const user = await User.create(req.body);
-  console.log(user);
-  res.status(201);
-  res.send({ message: 'User created' });
 });
 
 export default app;
