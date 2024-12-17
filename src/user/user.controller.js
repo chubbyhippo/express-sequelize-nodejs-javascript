db4e23df7440071e9d542bdb9c831d7c8740a7d0
ch <mkmsacc@outlook.com>
2024-12-17 15:23:39 +0700

    Remove redundant username null check in createUser method.
    The validation for a null username was removed as it is unnecessary. This simplifies the code and offloads validation responsibility to the userService or other validation layers.

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -2,11 +2,6 @@ import userService from './user.service.js';
 
 class UserController {
   createUser = () => async (req, res) => {
-    const user = req.body;
-    if (user.username === null) {
-      res.status(400).send({ validationErrors: {} });
-      return;
-    }
     await userService.createUser(req.body);
     res.status(201);
     res.send({ message: 'UserEntity created' });
