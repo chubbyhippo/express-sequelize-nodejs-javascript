73d4685fb4c89685b8c708f9a09df6ffadcc9f77
ch <mkmsacc@outlook.com>
2024-12-20 09:50:41 +0700

    Add i18n support with i18next and middleware integration
    Integrated i18next for internationalization and configured it with file system backend and language detection. Middleware was added to handle translations, with a default fallback language set to English. This sets up the foundation for multi-language support in the app.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@ import express from 'express';
 import userRouter from './user/user.router.js';
 import console from 'node:console';
 import process from 'node:process';
+import i18next from 'i18next';
+import Backend from 'i18next-fs-backend';
+import middleware from 'i18next-http-middleware';
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    lng: 'en',
+    ns: ['translation'],
+    defaultNS: 'translation',
+    backend: {
+      loadPath: './locales/{{lng}}/{{ns}}.json',
+    },
+    detection: {
+      lookupHeader: 'Accept-Language',
+    },
+  });
 
 const app = express();
 
+app.use(middleware.handle(i18next));
+
 app.use(express.json());
+
 app.use(userRouter);
 
 app.get('/', (req, res) => {
