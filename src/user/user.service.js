bfcee36867c3c66d0f62191fbf5fb193264b305c
ch <mkmsacc@outlook.com>
2024-12-23 10:57:29 +0700

    Add default inactive status when creating a user
    Changed the createUser method to include a default `inactive` status for newly created users. This ensures that accounts require activation or further actions before becoming active. Simplifies the creation process by using spread syntax for user object properties.

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -3,9 +3,8 @@ import userRepository from './user.repository.js';
 class UserService {
   async createUser(user) {
     return await userRepository.create({
-      username: user.username,
-      password: user.password,
-      email: user.email,
+      ...user,
+      inactive: true,
     });
   }
 }
