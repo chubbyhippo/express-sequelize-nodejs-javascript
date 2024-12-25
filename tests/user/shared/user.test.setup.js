6e6b40e17014992344c67b251d377a8b9ef9b0f3
ch <mkmsacc@outlook.com>
2024-12-25 12:43:04 +0700

    Add test setup file for user-related tests
    Introduce a reusable test setup file to configure the test environment for user-related tests. This includes server initialization, database synchronization, and predefined valid user inputs.

---
+++ tests/user/shared/user.test.setup.js
@@ -0,0 +1,33 @@
+import { afterAll, beforeAll, beforeEach } from 'vitest';
+import { createServer } from 'http';
+import app from '../../../src/app.js';
+import sequelize from '../../../src/config/database.js';
+import UserEntity from '../../../src/user/user.entity.js';
+
+let server;
+let baseUrl;
+
+beforeAll(async () => {
+  server = createServer(app);
+  await server.listen(0);
+
+  await sequelize.sync({ force: true });
+
+  baseUrl = `http://localhost:${server.address().port}`;
+});
+
+afterAll(async () => {
+  server.close();
+});
+
+beforeEach(async () => {
+  await UserEntity.sync({ force: true });
+});
+
+const validUserInputs = {
+  username: 'test',
+  password: 'P4ssw0rd',
+  email: 'test@test.com',
+};
+
+export { baseUrl, validUserInputs };
