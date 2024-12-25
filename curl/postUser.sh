ba002f460a2cf199b6e9ce2a4e847eabfec78425
ch <mkmsacc@outlook.com>
2024-12-25 09:59:19 +0700

    Remove unnecessary User-Agent header in postUser.sh
    The User-Agent header has been removed to simplify the curl request. This change ensures the script focuses solely on essential headers, improving maintainability.

--- curl/postUser.sh
+++ curl/postUser.sh
@@ -3,7 +3,6 @@
 curl --request POST \
   --url http://localhost:8080/api/users \
   --header 'Content-Type: application/json' \
-  --header 'User-Agent: insomnia/10.0.0' \
   --data '{
 	"username": "username",
 	"password": "P4ssword",
