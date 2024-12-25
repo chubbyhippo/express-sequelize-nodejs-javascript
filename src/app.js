064e5ccd5420212cf7e440b1660632cb34b0a9df
ch <mkmsacc@outlook.com>
2024-12-25 12:30:53 +0700

    Refactor user routing to consolidate logic in user controller.
    Replaced `app.router.js` with an integrated router directly in `user.controller.js` to simplify the routing structure. Moved user validation and user creation logic into the consolidated controller for better maintainability and clarity. Updated `app.js` to reflect the refactored routing setup.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import router from './app.router.js';
 import console from 'node:console';
 import process from 'node:process';
 import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 import i18nextMiddleware from 'i18next-http-middleware';
+import userController from './user/user.controller.js';
 
 // noinspection JSIgnoredPromiseFromCall
 i18next
@@ -29,7 +29,7 @@ app.use(i18nextMiddleware.handle(i18next));
 
 app.use(express.json());
 
-app.use(router);
+app.use(userController);
 
 console.log(`Environment: ${process.env.NODE_ENV}`);
 
--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,18 +1,26 @@
 import userService from './user.service.js';
 import console from 'node:console';
+import { userValidationRules, validate } from './user.validation.js';
+import express from 'express';
 
-class UserController {
-  createUser = () => async (req, res) => {
+const router = express.Router();
+
+router.post(
+  '/api/users',
+  ...userValidationRules(),
+  validate,
+  async (req, res) => {
     try {
       await userService.createUser(req.body);
       res.status(201);
+      // noinspection JSCheckFunctionSignatures
       res.send({ message: req.t('userCreated') });
     } catch (error) {
       res.status(502);
       console.error(error);
+      // noinspection JSCheckFunctionSignatures
       res.send({ message: req.t('userCreationFailed') });
     }
-  };
-}
-
-export default new UserController();
+  }
+);
+export default router;
