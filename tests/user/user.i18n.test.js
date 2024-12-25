8ab5a4b079396517f176d12c927a47a02908faea
ch <mkmsacc@outlook.com>
2024-12-25 12:37:04 +0700

    ``` Refactor i18n tests into a dedicated file.
    Moved i18n tests from user.registration.test.js to a new file, user.i18n.test.js, for improved modularity and readability. Ensured test setup and structure remain consistent across the refactored files.
    ```

---
+++ tests/user/user.i18n.test.js
@@ -0,0 +1,153 @@
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest';
+import axios from 'axios';
+import userRepository from '../../src/user/user.repository.js';
+import console from 'node:console';
+import { createServer } from 'http';
+import app from '../../src/app.js';
+import sequelize from '../../src/config/database.js';
+import UserEntity from '../../src/user/user.entity.js';
+
+let server;
+let baseUrl;
+
+beforeAll(async () => {
+  server = createServer(app);
+  await server.listen(0);
+
+  await sequelize.sync({ force: true });
+
+  baseUrl = `http://localhost:${server.address().port}`;
+});
+
+afterAll(async () => {
+  server.close();
+});
+
+beforeEach(async () => {
+  await UserEntity.sync({ force: true });
+});
+
+const validUserInputs = {
+  username: 'test',
+  password: 'P4ssw0rd',
+  email: 'test@test.com',
+};
+
+describe('i18n test', () => {
+  const postForUser = async (userInputs) =>
+    await axios.post(`${baseUrl}/api/users`, userInputs);
+
+  const usernameNull = 'Username is required';
+  const usernameLength = 'Username must be between 4 and 32 characters long';
+  const passwordNull = 'Password is required';
+  const passwordLength = 'Password must be between 6 and 32 characters long';
+  const passwordPattern =
+    'Password must contain at least one uppercase letter, one lowercase letter, and one number';
+  const emailNull = 'Email is required';
+  const emailInvalid = 'Email must be valid';
+  const emailRegistered = 'Email is already registered';
+
+  it.each`
+    field         | value                  | expectedErrorMessage
+    ${'username'} | ${null}                | ${usernameNull}
+    ${'username'} | ${'usr'}               | ${usernameLength}
+    ${'username'} | ${'u'.repeat(33)}      | ${usernameLength}
+    ${'password'} | ${null}                | ${passwordNull}
+    ${'password'} | ${'p'.repeat(5)}       | ${passwordLength}
+    ${'password'} | ${'lowercase'}         | ${passwordPattern}
+    ${'password'} | ${'UPPERCASE'}         | ${passwordPattern}
+    ${'password'} | ${'UPPERandlowercase'} | ${passwordPattern}
+    ${'email'}    | ${null}                | ${emailNull}
+    ${'email'}    | ${'test.com'}          | ${emailInvalid}
+  `(
+    `should return error message: $expectedErrorMessage for field: $field with value: $value`,
+    async ({ field, value, expectedErrorMessage }) => {
+      const input = {
+        username: 'test',
+        password: 'P4ssword',
+        email: 'test@test.com',
+      };
+
+      input[field] = value;
+      let errorMessage;
+      await postForUser(input).catch((error) => {
+        const validationErrors = error.response.data.validationErrors;
+        errorMessage = validationErrors[field];
+      });
+      expect(errorMessage).toBe(expectedErrorMessage);
+    }
+  );
+
+  it('should return email is already registered when email is already registered', async () => {
+    await userRepository.create(validUserInputs);
+    let errorMessage;
+    await postForUser(validUserInputs).catch((error) => {
+      const validationErrors = error.response.data.validationErrors;
+      errorMessage = validationErrors.email;
+    });
+    expect(errorMessage).toBe(emailRegistered);
+  });
+});
+
+describe('i18n test in Chinese', () => {
+  const postForUser = async (userInputs) =>
+    await axios.post(`${baseUrl}/api/users`, userInputs, {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Content-Language': 'zh',
+        'Accept-Language': 'zh',
+      },
+    });
+
+  const usernameNull = '用户名是必需的';
+  const usernameLength = '用户名长度必须在4到32个字符之间';
+  const passwordNull = '密码是必需的';
+  const passwordLength = '密码长度必须在6到32个字符之间';
+  const passwordPattern =
+    '密码必须包含至少一个大写字母、一个小写字母和一个数字';
+  const emailNull = '邮箱是必需的';
+  const emailInvalid = '邮箱必须是有效的';
+  const emailRegistered = '邮箱已被注册';
+
+  it.each`
+    field         | value                  | expectedErrorMessage
+    ${'username'} | ${null}                | ${usernameNull}
+    ${'username'} | ${'usr'}               | ${usernameLength}
+    ${'username'} | ${'u'.repeat(33)}      | ${usernameLength}
+    ${'password'} | ${null}                | ${passwordNull}
+    ${'password'} | ${'p'.repeat(5)}       | ${passwordLength}
+    ${'password'} | ${'lowercase'}         | ${passwordPattern}
+    ${'password'} | ${'UPPERCASE'}         | ${passwordPattern}
+    ${'password'} | ${'UPPERandlowercase'} | ${passwordPattern}
+    ${'email'}    | ${null}                | ${emailNull}
+    ${'email'}    | ${'test.com'}          | ${emailInvalid}
+  `(
+    `should return error message: $expectedErrorMessage for field: $field with value: $value`,
+    async ({ field, value, expectedErrorMessage }) => {
+      const input = {
+        username: 'test',
+        password: 'P4ssword',
+        email: 'test@test.com',
+      };
+
+      input[field] = value;
+      let errorMessage;
+      await postForUser(input).catch((error) => {
+        const validationErrors = error.response.data.validationErrors;
+        errorMessage = validationErrors[field];
+      });
+      console.log(errorMessage);
+      expect(errorMessage).toBe(expectedErrorMessage);
+    }
+  );
+
+  it(`should return message: ${emailRegistered} when email is already registered`, async () => {
+    await userRepository.create(validUserInputs);
+    let errorMessage;
+    await postForUser(validUserInputs).catch((error) => {
+      const validationErrors = error.response.data.validationErrors;
+      errorMessage = validationErrors.email;
+    });
+    expect(errorMessage).toBe(emailRegistered);
+  });
+});
