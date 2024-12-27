966f3310a489c8eae901daa89eca071b3c2f27fb
ch <mkmsacc@outlook.com>
2024-12-27 13:07:15 +0700

    Refactor tests to remove unused email server setup.
    Eliminated the SMTP email server setup from user test configuration, as it was no longer in use. This reduces unnecessary complexity and improves maintainability of the test suite.

--- tests/user/shared/user.test.setup.js
+++ tests/user/shared/user.test.setup.js
@@ -3,16 +3,11 @@ import { createServer } from 'http';
 import app from '../../../src/app.js';
 import sequelize from '../../../src/config/database.js';
 import UserEntity from '../../../src/user/user.entity.js';
-import { SMTPServer } from 'smtp-server';
-import console from 'node:console';
 
 const serverPort = 0;
-const emailServerPort = 2525;
 
 let server;
 let baseUrl;
-let emailServer;
-let lastMail;
 
 beforeAll(async () => {
   server = createServer(app);
@@ -21,27 +16,9 @@ beforeAll(async () => {
   await sequelize.sync({ force: true });
 
   baseUrl = `http://localhost:${server.address().port}`;
-
-  // noinspection JSUnusedGlobalSymbols
-  emailServer = new SMTPServer({
-    authOptional: true,
-    onData: (stream, session, callback) => {
-      let mailBody;
-      stream.on('data', (chunk) => {
-        console.log(chunk.toString());
-        mailBody += chunk.toString();
-      });
-      stream.on('end', () => {
-        lastMail = mailBody;
-        callback();
-      });
-    },
-  });
-  await emailServer.listen(emailServerPort, 'localhost');
 });
 
 afterAll(async () => {
-  await emailServer.close();
   server.close();
 });
 
@@ -55,4 +32,4 @@ const validUserInputs = {
   email: 'test@test.com',
 };
 
-export { baseUrl, validUserInputs, lastMail };
+export { baseUrl, validUserInputs };
